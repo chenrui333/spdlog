7fe63d68188886d6c10fc4d6777ef54373cfedca
gabime <gmelman1@gmail.com>
2024-12-06 15:21:18 +0200

    Clean context

--- src/spdlog.cpp
+++ src/spdlog.cpp
@@ -12,11 +12,12 @@
 
 namespace spdlog {
 
-#ifndef SPDLOG_DISABLE_GLOBAL_LOGGER
+
 static std::shared_ptr s_context =
+#ifndef SPDLOG_DISABLE_GLOBAL_LOGGER
     std::make_unique<details::context>(std::make_unique<logger>("", std::make_unique<sinks::stdout_color_sink_mt>()));
 #else
-static std::shared_ptr s_context = std::make_unique<details::context>();
+    std::make_unique<details::context>(); // empty context
 #endif
 
 void set_context(std::shared_ptr<details::context> context) { s_context = std::move(context); }
