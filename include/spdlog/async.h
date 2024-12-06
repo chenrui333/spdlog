fbfe2460758439c2541b02d676d939244f8ecb5b
gabime <gmelman1@gmail.com>
2024-12-06 14:24:28 +0200

    context in async factory

--- include/spdlog/async.h
+++ include/spdlog/async.h
@@ -26,7 +26,7 @@
 namespace spdlog {
 
 namespace details {
-static const size_t default_async_q_size = 8192;
+static constexpr size_t default_async_q_size = 8192;
 }
 
 // async logger factory - creates async loggers backed with thread pool.
